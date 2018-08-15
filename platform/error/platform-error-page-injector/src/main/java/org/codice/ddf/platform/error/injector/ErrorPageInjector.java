3298ed20a00fefb13ce18c743f4193e051feaece
Kjames5269 <kjames5269@gmail.com>
2018-08-15 08:47:48 -0700

    DDF-4063 Adding the changes chris made, temp version

--- platform/error/platform-error-page-injector/src/main/java/org/codice/ddf/platform/error/injector/ErrorPageInjector.java
+++ platform/error/platform-error-page-injector/src/main/java/org/codice/ddf/platform/error/injector/ErrorPageInjector.java
@@ -17,19 +17,26 @@ import com.google.common.collect.ImmutableMap;
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import javax.servlet.ServletContext;
 import org.codice.ddf.platform.error.servlet.ErrorServlet;
 import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.*;
 import org.osgi.framework.hooks.service.EventListenerHook;
 import org.osgi.framework.hooks.service.ListenerHook.ListenerInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+/* TODO:
+   Unfortunatly the only way I've found to get from a servlet to a servletHolder is by using Field
+   ...
+   I don't like it but it may be worth abstracting that to a method.
+ */
+
 
 public class ErrorPageInjector implements EventListenerHook {
 
@@ -51,6 +58,20 @@ public class ErrorPageInjector implements EventListenerHook {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ErrorPageInjector.class);
 
+  private final ScheduledExecutorService executorService;
+
+  public ErrorPageInjector(ScheduledExecutorService executorService) {
+    this.executorService = executorService;
+  }
+
+  BundleContext getContext() {
+    final Bundle cxfBundle = FrameworkUtil.getBundle(ErrorPageInjector.class);
+    if (cxfBundle != null) {
+      return cxfBundle.getBundleContext();
+    }
+    return null;
+  }
+
   @Override
   public void event(ServiceEvent event, Map<BundleContext, Collection<ListenerInfo>> listeners) {
     if (event.getType() == ServiceEvent.REGISTERED) {
@@ -60,6 +81,44 @@ public class ErrorPageInjector implements EventListenerHook {
       if (service instanceof ServletContext) {
         injectErrorPage((ServletContext) service, refBundle);
       }
+    }
+  }
+
+  public void init() {
+    executorService.schedule(this::checkForMissedServletContexts,1, TimeUnit.SECONDS);
+  }
+
+  private void checkForMissedServletContexts() {
+    try {
+      BundleContext context = getContext();
+      if (context == null) {
+        return; // bundle is probably refreshing
+      }
+      Collection<ServiceReference<ServletContext>> references =
+              context.getServiceReferences(ServletContext.class, null);
+      for (ServiceReference<ServletContext> reference : references) {
+        Bundle refBundle = reference.getBundle();
+        BundleContext bundlectx = refBundle.getBundleContext();
+        ServletContext service = bundlectx.getService(reference);
+
+        if (service.getFilterRegistration(DELEGATING_FILTER) == null) {
+          LOGGER.error(
+                  "Platform filter delegate failed to start in time to inject itself into {} {}. This means the {} servlet will not properly attach the user subject to requests. Attempting to resolve the issue by restarting the bundle.",
+                  refBundle.getSymbolicName(),
+                  refBundle.getBundleId(),
+                  refBundle.getSymbolicName());
+
+          // Restarting the missed servlet context bundle so the injector will be able to perform
+          // its job.
+          refBundle.stop();
+          refBundle.start();
+        }
+      }
+
+    } catch (InvalidSyntaxException | BundleException e) {
+      LOGGER.error(
+              "Problem checking ServletContexts for DelegateServletFilter injections. One of the servlets running might not have all of the needed filters injected. A system restart is recommended. See debug logs for additional details.");
+      LOGGER.debug("Additional Details:", e);
     }
   }
 
